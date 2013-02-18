94e5287d4541c231f1c842a04c0200af08c49489
Martin-Louis Bright <mlbright@gmail.com>
2013-02-18 00:18:32 -0500

    unitlist

--- sugoku.go
+++ sugoku.go
@@ -3,7 +3,28 @@ package main
 import "fmt"
 import "strings"
 
-/* All this really could be much nicer, because I really don't know what I'm doing with golang yet. */
+type unit []string
+type unitgroup []unit
+
+var rows string
+var cols string
+var digits string
+var squares []string
+var unitlist []unit
+var units map[string]unitgroup
+
+func test() {
+
+    if len(squares) != 81 {
+        panic("wtf, the number of squares is not 81")
+    }
+
+    if len(unitlist) != 27 {
+        fmt.Println("The size of unitlist is",len(unitlist))
+        panic("wtf, the number of units is not 27")
+    }
+
+}
 
 func cross(x string, y string) []string {
     result := make([]string,0)
@@ -19,11 +40,42 @@ func cross(x string, y string) []string {
 }
 
 func main() {
-    rows := "ABCDEFGHI"
-    digits := "123456789"
-    cols := digits
-    squares := cross(rows,cols)
-    for _,i := range squares {
-        fmt.Println(i)
+    rows = "ABCDEFGHI"
+    digits = "123456789"
+    cols = digits
+    squares = cross(rows,cols)
+
+    unitlist = make([]unit,0)
+
+    for _,c := range cols {
+        unitlist = append(unitlist,cross(rows,string(c)))
     }
+    for _,r := range rows {
+        unitlist = append(unitlist,cross(string(r),cols))
+    }
+    rs := []string{"ABC","DEF","GHI"}
+    cs := []string{"123","456","789"}
+
+    for _,r := range rs {
+        for _,c := range cs {
+            unitlist = append(unitlist,cross(r,c))
+        }
+    }
+
+    units = make(map[string]unitgroup)
+    for _,s := range squares {
+        group := make(unitgroup,0)
+        for _,unit := range unitlist {
+            for _,s2 := range unit {
+                if s2 == s {
+                    group = append(group,unit)
+                    break
+                }
+            }
+        }
+        units[s] = group
+    }
+    
+    fmt.Println(units)
+    test()
 }
